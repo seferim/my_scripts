6834b2e4f31abd417ad359157a0a41156feee860
Panagiotis Filippakopoulos <filippakpan@gmail.com>
2025-05-01 23:22:00 +0300

    Refactor: inclusion of optional autoremove before upgrading, comment correction and 'code' structure changes for clarity.

--- auua.sh
+++ auua.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-: '   Removal of packages that are no longer needed
-   (installed mostly to satisfy dependencies),
-   then download packages information and install
-   any available upgrades. Finally repeate the 
-   first action again. '
+# Debian based systems maintenance.
+# Optional removal of unnecessary packages before upgrading.
+# After updating the package list and upgrading the installed packages it cleans up unnecessary software.
+    
 
-sudo apt autoremove -y ; sudo apt update -y ; sudo apt upgrade -y ; sudo apt-get dist-upgrade && sudo apt autoremove -y
+   echo "Clean up unused dependincies before upgrading? (y/n)"
+   read -r anwser
+
+# Convert input to lowercase and if it starts with 'y'.
+   if echo "$anwser" | grep -iq "^y"; then
+	   sudo apt autoremove -y
+   fi
+
+sudo apt update && \
+sudo apt upgrade -y && \
+sudo apt full-upgrade -y && \
+sudo apt autoremove -y
